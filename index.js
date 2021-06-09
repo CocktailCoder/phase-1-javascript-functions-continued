048cde50aaea000699007fd14dea965005979f04
cocktailcoder <iokreddit@protonmail.com>
2021-06-09 11:27:00 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun (act ='roller-skate'){
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(todo='go to the office'){
+    return `This Monday, I will ${todo}.`
+}
+
+function wrapAdjective(initialize = "*",){
+    return function (string = 'special'){
+        return (`You are ${initialize + string + initialize}!`);
+    }
+};
